6dab2d9bed4d54691fff039becef21ffdbff1129
Akchu <iamakshaybisht@gmail.com>
2017-07-19 22:00:57 +0530

    First Upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody rb;
+	private AudioSource AudioS;
+
+	public GameObject soldier;
+
+
+	int foot=0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		AudioS = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+//		if (!soldier.activeInHierarchy)
+//			return;
+//		else 
+//		{
+			float x = CrossPlatformInputManager.GetAxis ("Horizontal");
+			float y = CrossPlatformInputManager.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3 (x, 0, y);
+			rb.velocity = movement * 1f;
+
+			if (x != 0 && y != 0)
+				transform.eulerAngles = new Vector3 (transform.eulerAngles.x, Mathf.Atan2 (x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
+
+			if (x != 0 && y != 0) 
+			{
+				
+				soldier.GetComponent<Animation> ().Play ("Walk Aiming");
+
+				//Audio Footsteps
+				if (AudioS.isPlaying == false && foot < 2) 
+				{
+					AudioS.volume = Random.Range (0.8f, 1);
+					AudioS.pitch = Random.Range (0.8f, 1.1f);
+					AudioS.Play ();
+				}
+			} 
+			else
+				soldier.GetComponent<Animation> ().Play ("Idle Aim");
+			
+			foot++;
+			if (foot > 1)
+				foot = 0;
+//		}
+	}
+
+	public void LostPlayer()
+	{
+		rb.transform.position = new Vector3 (0f, 0f, 0f);
+	}
+
+
+
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour 
+{
+	public GameObject bullet;
+	private Vector2 touchOrigin = -Vector2.one;
+	//public Transform spawnPos;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		int horizontal = 0;
+		int vertical = 0;
+
+//		#if UNITY_EDITOR || UNITY_STANDALONE
+//
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			GameObject go = Instantiate (bullet, transform.position, transform.rotation)as GameObject;
+			//GameObject go = Instantiate (bullet, spawnPos.position, spawnPos.rotation)as GameObject;
+			go.GetComponent<Rigidbody> ().AddForce (transform.forward * 15, ForceMode.Impulse);
+		}
+
+			
+//		if (Input.touchCount > 0) 
+//		{
+//			Touch myTouch = Input.touches[0];
+//			if (myTouch.phase == TouchPhase.Began)
+//				touchOrigin = myTouch.position;
+//			else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0) 
+//			{
+//				Vector2 touchEnd = myTouch.position;
+//				float x = touchEnd.x - touchOrigin.x;
+//				float y = touchEnd.y - touchOrigin.y;
+//				touchOrigin.x = -1;
+//				if (Mathf.Abs (x) > Mathf.Abs (y))
+//					horizontal = x > 0 ? 1 : -1;
+//				else
+//					vertical = y > 0 ? 1 : -1;
+//			}
+//		}
+//
+//		if (horizontal != 0 || vertical != 0) 
+//		{
+//			GameObject go = Instantiate (bullet, transform.position, transform.rotation)as GameObject;
+//			//GameObject go = Instantiate (bullet, spawnPos.position, spawnPos.rotation)as GameObject;
+//			go.GetComponent<Rigidbody> ().AddForce (transform.forward * 50, ForceMode.Impulse);	
+//		} 
+
+	}
+}
